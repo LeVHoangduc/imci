d8823635f52ef0b66eea46ff3ed1ee47d10716cd
Thuan Nguyen <thuannguyenhuu11@gmail.com>
2024-12-14 16:50:52 +0700

    feat: add UI for child page

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,11 +1,22 @@
+"use client";
+
 import Image from "next/image";
 import React from "react";
+import { usePathname } from "next/navigation";
+import { ArrowLeft } from "lucide-react";
 
 const NavBar = () => {
+  const pathname = usePathname();
+
   return (
     <div className="flex w-full py-3 pl-3 bg-[#03DAC5]">
       <div className="flex gap-5 items-center">
-        <Image src="/logo.png" width={40} height={40} alt="logo" />
+        {pathname === "/" && (
+          <Image src="/logo.png" width={40} height={40} alt="logo" />
+        )}
+
+        {pathname !== "/" && <ArrowLeft />}
+
         <p className="font-bold text-lg">IMCI</p>
       </div>
     </div>
