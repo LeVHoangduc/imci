3e6460f83055587403c815f002023cf2077c8406
Thuan Nguyen <thuannguyenhuu11@gmail.com>
2024-12-15 20:51:19 +0700

    feat: add auth feature

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,54 +1,68 @@
-import Image from "next/image";
-import Link from "next/link";
+'use client';
+import Image from 'next/image';
+import { useAuth } from '@/provider/AuthProvider';
+import { useRouter } from 'next/navigation';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function Home() {
-  return (
-    <div className="bg-gray-200 h-screen pt-5 px-5 flex flex-col md:flex-row gap-10">
-      <div className="flex flex-col w-full">
-        <h2 className="font-bold text-gray-500">Assess and classify</h2>
-        <div className="mt-5 flex w-full gap-4">
-          <Link href="/infant" className="w-1/2 max-w-[300px] min-h-[150px]">
-            <button
-              type="button"
-              className="flex flex-col items-center justify-center border rounded-lg bg-white p-4 cursor-pointer gap-4 h-full"
-            >
-              <Image src="/infant-icon.png" width={50} height={50} alt="" />
-              <p className="">Sick young infant (up to 2 months old)</p>
-            </button>
-          </Link>
+    const { isLoggedIn } = useAuth();
+    const router = useRouter();
+    console.log(isLoggedIn);
 
-          <Link href="/child" className="w-1/2 max-w-[300px] min-h-[150px]">
-            <button
-              type="button"
-              className="flex flex-col items-center justify-center border rounded-lg bg-white p-4 cursor-pointer gap-4 h-full"
-            >
-              <Image src="/children-icon.png" width={50} height={50} alt="" />
-              <p className="">Sick child (2 months up to 5 years old)</p>
-            </button>
-          </Link>
+    const handleToPageInfant = () => {
+        if (isLoggedIn) {
+            router.push('/infant');
+        } else {
+            toast.success('Vui lòng đăng nhập');
+            router.push('/login');
+        }
+    };
+
+    return (
+        <div className="bg-gray-200 h-screen pt-5 px-5 flex flex-col md:flex-row gap-10">
+            <div className="flex flex-col w-full">
+                <h2 className="font-bold text-gray-500 uppercase">Đánh giá và phân loại</h2>
+                <div className="mt-5 flex w-full gap-4">
+                    <button
+                        type="button"
+                        className="w-1/2 max-w-[300px] min-h-[150px] flex flex-col items-center justify-center border rounded-lg bg-white p-4 cursor-pointer gap-4 h-full"
+                        onClick={handleToPageInfant}
+                    >
+                        <Image src="/infant-icon.png" width={50} height={50} alt="" />
+                        <p className="">Trẻ ốm (dưới 2 tháng tuổi)</p>
+                    </button>
+
+                    <button
+                        type="button"
+                        className="w-1/2 max-w-[300px] min-h-[150px] flex flex-col items-center justify-center border rounded-lg bg-white p-4 cursor-pointer gap-4 h-full"
+                    >
+                        <Image src="/children-icon.png" width={50} height={50} alt="" />
+                        <p className="">Trẻ ốm (từ 2 tháng đến 5 tuổi)</p>
+                    </button>
+                </div>
+            </div>
+
+            <div className="flex flex-col w-full">
+                <h2 className="font-bold text-gray-500">Settings</h2>
+                <div className="mt-5 flex w-full gap-4">
+                    <button
+                        type="button"
+                        className="w-1/2 flex flex-col items-center justify-center border rounded-lg bg-white p-4 cursor-pointer gap-4 h-full max-w-[300px] min-h-[150px]"
+                    >
+                        <Image src="/theme-icon.png" width={50} height={50} alt="" />
+                        <p className="">App Theme</p>
+                    </button>
+
+                    <button
+                        type="button"
+                        className="w-1/2 flex flex-col items-center justify-center border rounded-lg bg-white p-4 cursor-pointer gap-4 h-full max-w-[300px] min-h-[150px]"
+                    >
+                        <Image src="/children-icon.png" width={50} height={50} alt="" />
+                        <p className="">About</p>
+                    </button>
+                </div>
+            </div>
         </div>
-      </div>
-
-      <div className="flex flex-col w-full">
-        <h2 className="font-bold text-gray-500">Settings</h2>
-        <div className="mt-5 flex w-full gap-4">
-          <button
-            type="button"
-            className="w-1/2 flex flex-col items-center justify-center border rounded-lg bg-white p-4 cursor-pointer gap-4 h-full max-w-[300px] min-h-[150px]"
-          >
-            <Image src="/theme-icon.png" width={50} height={50} alt="" />
-            <p className="">App Theme</p>
-          </button>
-
-          <button
-            type="button"
-            className="w-1/2 flex flex-col items-center justify-center border rounded-lg bg-white p-4 cursor-pointer gap-4 h-full max-w-[300px] min-h-[150px]"
-          >
-            <Image src="/children-icon.png" width={50} height={50} alt="" />
-            <p className="">About</p>
-          </button>
-        </div>
-      </div>
-    </div>
-  );
+    );
 }
